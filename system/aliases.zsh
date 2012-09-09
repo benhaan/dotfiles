c06cc00aabfb69fa8c2fd231d3d685c94dc92f6c
Ben Haan <benhaan@gmail.com>
2012-09-09 17:59:00 -0500

    added shortcuts for sublime and a couple mac only utilities

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,17 @@ then
   alias ll="gls -l --color"
   alias la="gls -A --color"
 fi
+
+# use MacVim
+mvim_path=$(which mvim)
+if [[ -f $mvim_path ]]
+then
+  alias vim=$mvim_path
+fi
+
+# flush mac dns cache
+dscacheutil_path=$(which dscacheutil)
+if [[ -f $dscacheutil_path ]]
+then
+  alias flushdns='dscacheutil -flushcache'
+fi
