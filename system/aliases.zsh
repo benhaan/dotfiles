8c5981964946c83dae1389726a517e97ea3fef36
Ben Haan <benhaan@gmail.com>
2017-01-06 11:18:19 -0600

    updates

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,13 +9,6 @@ then
   alias la="gls -A --color"
 fi
 
-# use MacVim
-#mvim_path=$(which mvim)
-#if [[ -f $mvim_path ]]
-#then
-#  alias vim=$mvim_path
-#fi
-
 # flush mac dns cache
 dscacheutil_path=$(which dscacheutil)
 if [[ -f $dscacheutil_path ]]
--- system/env.zsh
+++ system/env.zsh
@@ -1,16 +1,4 @@
 export EDITOR="vim"
 export NODE_PATH="/usr/local/share/npm/lib/node_modules"
 
-export VAGRANT_HOME="/Volumns/Universe/Vagrant"
-
-if [[ -d ~/.ec2 ]]
-then
-  # AWS Tools
-  export JAVA_HOME="$(/usr/libexec/java_home)"
-  export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-  export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-  export EC2_HOME="/opt/boxen/homebrew/Library/LinkedKegs/ec2-api-tools/libexec"
-  export EC2_AMITOOL_HOME="/opt/boxen/homebrew/Library/LinkedKegs/ec2-ami-tools/libexec"
-  export AWS_ELB_HOME="/opt/boxen/homebrew/Library/LinkedKegs/elb-tools/libexec"
-  export AWS_RDS_HOME="/opt/boxen/homebrew/Library/LinkedKegs/rds-command-line-tools/libexec"
-fi
+export VAGRANT_HOME="/mnt/Universe/Vagrant"
